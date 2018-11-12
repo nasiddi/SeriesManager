e4691bbdd236eac175c0c6193fb5493693377bbd
nasiddi <nadinasiddiqui@msn.com>
2018-11-12 21:57:26 +0100

    log

--- backend/syncer.py
+++ backend/syncer.py
@@ -54,7 +54,8 @@ def main(args):
 
     for file in files:
         if file.delete:
-            CLEAN_UP.append(SEPERATOR.join(file.location.split(SEPERATOR)[:3 + MAC_OFFSET]))
+            CLEAN_UP.append(SEPERATOR.join(
+                file.location.split(SEPERATOR)[:3 + MAC_OFFSET]))
             continue
         if file.type_option == '[ignore]':
             ignore_file(file)
@@ -68,20 +69,20 @@ def main(args):
             queue_episode(file)
             continue
 
-
     sync_queue()
     clean_up()
     report = []
     for file in QUEUE:
         report.append(file.get_report())
-    log = io_utlis.load_json(os.path.join(os.path.dirname(os.environ['OUTPUT_FILE']), 'synclog'))
+    log = io_utlis.load_json(os.path.join(
+        os.path.dirname(os.environ['OUTPUT_FILE']), 'synclog'))
     print(log)
     log.extend(report)
     io_utlis.save_json(report, os.environ['OUTPUT_FILE'])
-    io_utlis.save_json(log, os.path.dirname(os.environ['OUTPUT_FILE']), 'synclog'))
+    io_utlis.save_json(log, os.path.join(os.path.dirname(
+        os.environ['OUTPUT_FILE']), 'synclog'))
     print(json.dumps(report, indent=4, sort_keys=True))
     io_utlis.save_shows(SHOWS)
-
 
 
 def sync_queue():
@@ -136,7 +137,8 @@ def delete_file(file):
     location = None
     if file.type_option == 'Series':
         try:
-            location = SHOWS[file.series_name].get_episode_by_sxe(file.s_nr, file.e_nr).location
+            location = SHOWS[file.series_name].get_episode_by_sxe(
+                file.s_nr, file.e_nr).location
         except KeyError:
             file.report['error'].append('Delete failed')
             return
@@ -170,7 +172,7 @@ def create_new_series(file):
     print(get_basepath(file).rsplit(SEPERATOR, 1))
     basepath = get_basepath(file).rsplit(SEPERATOR, 1)[0]
     SHOWS.update({file.series_name: Series(series_name=file.series_name, status=file.status, tvdb_id=file.tvdb_id,
-                  anime=file.anime, name_needed=file.name_needed, location=basepath)})
+                                           anime=file.anime, name_needed=file.name_needed, location=basepath)})
     file.report['info'].append('Series created')
 
 
@@ -197,12 +199,14 @@ def queue_episode(file):
 
 
 def queue_movie(file):
-    file.new_location = os.path.join(HD_Movies if file.type_option == 'HD' else SD_MOVIES, '{f.title}.{f.extention}'.format(f=file))
+    file.new_location = os.path.join(
+        HD_Movies if file.type_option == 'HD' else SD_MOVIES, '{f.title}.{f.extention}'.format(f=file))
     QUEUE.append(file)
 
 
 def get_basepath(file):
-    basepath = os.path.join(ANIME_DIR if file.anime else SERIES_DIR, file.series_name, 'Season {:02d}'.format(file.s_nr))
+    basepath = os.path.join(ANIME_DIR if file.anime else SERIES_DIR,
+                            file.series_name, 'Season {:02d}'.format(file.s_nr))
     if not os.path.exists(basepath):
         os.makedirs(basepath)
     io_utlis.wait_on_creation(basepath)
@@ -249,7 +253,8 @@ def triple_format(file):
     if not file.title == file.title2 and not file.title == file.title3 and not (file.title == '' or file.title2 == '' or file.title3 == ''):
         return '{f.series_name} {f.s_nr:02d}x{f.e_nr:0{p}d} & {f.s_nr:02d}x{f.e_nr2:0{p}d} & {f.s_nr:02d}x{f.e_nr3:0{p}d} - {f.title} & {f.title2} & {f.title3}.{f.extention}'\
                .format(f=file, p=3 if file.anime else 2)
-    title_set = list(filter(None, list({file.title, file.title2, file.title3})))
+    title_set = list(
+        filter(None, list({file.title, file.title2, file.title3})))
     # one title
     if len(title_set) == 1:
         return '{f.series_name} {f.s_nr:02d}x{f.e_nr:0{p}d} & {f.s_nr:02d}x{f.e_nr2:0{p}d} & {f.s_nr:02d}x{f.e_nr3:0{p}d} - {t}.{f.extention}' \
@@ -257,8 +262,6 @@ def triple_format(file):
     # two titles
     return '{f.series_name} {f.s_nr:02d}x{f.e_nr:0{p}d} & {f.s_nr:02d}x{f.e_nr2:0{p}d} & {f.s_nr:02d}x{f.e_nr3:0{p}d} - {t[0]} & {t[1]}.{f.extention}' \
         .format(f=file, p=3 if file.anime else 2, t=title_set)
-
-
 
 
 if __name__ == '__main__':
