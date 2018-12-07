1d12c73a87f6b3f3065b993dc110c33228de7fd9
nasiddi <nadinasiddiqui@msn.com>
2018-12-07 08:45:16 +0100

    pool

--- backend/missing_files.py
+++ backend/missing_files.py
@@ -7,11 +7,8 @@ from time import gmtime, strftime
 
 from tvdb_client import ApiV2Client
 
-from utils.constants import ENDED, SERIES_NAME, DOUBLE, TRIPLE, OUT_FILE
+from utils.constants import SERIES_NAME, DOUBLE, TRIPLE, OUT_FILE
 from utils.io_utlis import load_shows, parse_args, save_json
-
-SHOWS = None
-MISSING = []
 
 api_client = ApiV2Client('nadinasiddiquiwaz', 'ZEDKTMYBNB29LBOS', 'EISRLGJH035SO60Q')
 api_client.login()
@@ -20,68 +17,74 @@ DATE = int(strftime("%Y%m%d", gmtime()))
 
 
 def main(args):
-
-    global SHOWS
-    SHOWS = load_shows(read_only=True)
+    shows = load_shows(read_only=True)
     parse_args(args)
-    load_all()
-    save_json({'files': MISSING, 'info': 'No Missing Files'}, environ[OUT_FILE])
 
-
-def load_all():
-    shows = sorted(SHOWS.values(), key=get_series_name)
+    shows = sorted(shows.values(), key=get_series_name)
     p = multiprocessing.Pool(8)
-    p.map(get_show_data, shows)
+    missing_files = p.map(get_show_data, shows)
     p.close()
     p.join()
-    return
-    for show in sorted(SHOWS.values(), key=get_series_name):
-        get_show_data(show)
+    missing_files = [item for sublist in missing_files for item in sublist]
+    for i in range(len(missing_files)):
+        missing_files[i]['key'] = i
+
+    save_json({'files': missing_files, 'info': 'No Missing Files'}, environ[OUT_FILE])
 
 
 def get_show_data(show):
+    missing_files = []
     if show.tvdb_id:
-        check_for_newly_aired(show)
+        missing_files.extend(check_for_newly_aired(show))
     for season in show.seasons.values():
-        check_for_missing_season(show, season, sorted(show.seasons.values(), key=lambda x: x.s_nr))
+        missing_files.extend(check_for_missing_season(show, season, sorted(show.seasons.values(), key=lambda x: x.s_nr)))
         episodes = sorted(list(season.episodes.values()), key=lambda x: x.e_nr)
         for episode in episodes:
-            check_for_missing_episode(show, episode, episodes)
+            missing_files.extend(check_for_missing_episode(show, episode, episodes))
+    return missing_files
 
 
 def check_for_missing_season(show, s, seasons):
+    missing_files = []
     index = seasons.index(s)
     if index <= 0:
         if s.s_nr <= 1:
-            return
-        for nr in range(1, s.s_nr):
-            MISSING.append({'key': len(MISSING), SERIES_NAME: show.series_name, 's_nr': nr, 'e_nr': '*'})
+            return []
+        missing_files.extend(
+            [{'key': 0, SERIES_NAME: show.series_name, 's_nr': nr, 'e_nr': '*'}
+             for nr in range(1, s.s_nr)])
     last = seasons[index-1]
-    for nr in range(last.s_nr + 1, s.s_nr):
-        MISSING.append({'key': len(MISSING), SERIES_NAME: show.series_name, 's_nr': nr, 'e_nr': '*'})
+    missing_files.extend(
+        [{'key': 0, SERIES_NAME: show.series_name, 's_nr': nr, 'e_nr': '*'}
+         for nr in range(last.s_nr + 1, s.s_nr)])
+    return missing_files
 
 
 def check_for_missing_episode(show, e, episodes):
+    missing_files = []
     if e.e_nr >= 777:
-        return
+        return []
     index = episodes.index(e)
     if index <= 0:
         if e.e_nr <= 1:
-            return
-        for nr in range(1, e.e_nr):
-            MISSING.append({'key': len(MISSING), SERIES_NAME: show.series_name, 's_nr': e.s_nr, 'e_nr': nr})
+            return []
+        missing_files.extend(
+            [{'key': 0, SERIES_NAME: show.series_name, 's_nr': e.s_nr, 'e_nr': nr}
+             for nr in range(1, e.e_nr)])
     last = episodes[index-1]
     last_e_nr = last.e_nr
     if last.episode_option == DOUBLE:
         last_e_nr += 1
     elif last.episode_option == TRIPLE:
         last_e_nr += 2
-
-    for nr in range(last_e_nr + 1, e.e_nr):
-        MISSING.append({'key': len(MISSING), SERIES_NAME: show.series_name, 's_nr': e.s_nr, 'e_nr': nr})
+    missing_files.extend(
+        [{'key': 0, SERIES_NAME: show.series_name, 's_nr': e.s_nr, 'e_nr': nr}
+         for nr in range(last_e_nr + 1, e.e_nr)])
+    return missing_files
 
 
 def check_for_newly_aired(show):
+    missing_files = []
     episodes = []
     for i in range(1, 100):
         eps = api_client.get_series_episodes(show.tvdb_id, episode_number=None, page=i)
@@ -93,7 +96,7 @@ def check_for_newly_aired(show):
     missing_seasons = []
     if not episodes:
         print(show.series_name)
-        return
+        return []
     episodes = sorted(episodes, key=itemgetter('airedSeason', 'airedEpisodeNumber'), reverse=True)
     for e in episodes:
         if e['airedSeason'] == 0:
@@ -121,8 +124,11 @@ def check_for_newly_aired(show):
         missing.append({SERIES_NAME: show.series_name, 's_nr': e['airedSeason'], 'e_nr': e['airedEpisodeNumber']})
 
     if missing:
-        for m in reversed(missing):
-            MISSING.append({'key': len(MISSING), SERIES_NAME: m[SERIES_NAME], 's_nr': m['s_nr'], 'e_nr': m['e_nr']})
+        missing_files.extend(
+            [{'key': 0, SERIES_NAME: m[SERIES_NAME], 's_nr': m['s_nr'], 'e_nr': m['e_nr']}
+             for m in reversed(missing)])
+
+    return missing_files
 
 
 def get_series_name(show):
