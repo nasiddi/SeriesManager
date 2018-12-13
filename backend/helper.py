50422197027ff164d7c3646c3dd26999c734261c
Nadina Siddiqui <siddinad@students.zhaw.ch>
2018-12-13 15:33:02 +0100

    helper

--- backend/helper.py
+++ backend/helper.py
@@ -7,14 +7,24 @@ from series import Series
 from episode import Episode
 from test import *
 from shutil import move
+from tvdb_client import ApiV2Client
+
+from utils.constants import SERIES_NAME, DOUBLE, TRIPLE, OUT_FILE
+from utils.io_utlis import load_shows, parse_args, save_json
+
+shows = load_shows(read_only=True)
+
+api_client = ApiV2Client('nadinasiddiquiwaz', 'ZEDKTMYBNB29LBOS', 'EISRLGJH035SO60Q')
+api_client.login()
+
+for show in shows.values():
+    if not show.tvdb_id:
+        continue
+    api = api_client.get_series(show.tvdb_id)
+
+    api_name = api['seriesName']
+    if not show.series_name == api_name:
+        print(show.series_name, api_name, show.tvdb_id)
 
 
-d = 'assets/backup/20181211_142847'
-j_list = []
-for f in os.listdir(d):
-    p = os.path.join(d, f)
-    if 'json' in p:
-        j = load_json(p)
-        j_list.append({f: j})
 
-print(j_list)
