3045389f30912a1dfb4879e69c9a30f0c7c63f3f
siddinad <siddinad@students.zhaw.ch>
2019-01-08 15:02:08 +0100

    retina

--- backend/helper.py
+++ backend/helper.py
@@ -1,28 +1,42 @@
-import os
-from utils.io_utlis import *
-from utils.constants import *
-import error_search
-from os.path import join
-from series import Series
-from episode import Episode
-from shutil import move
+from operator import itemgetter
 from tvdb_client import ApiV2Client
-
+from series import Series
 from utils.constants import SERIES_NAME, DOUBLE, TRIPLE, OUT_FILE
 from utils.io_utlis import load_shows, parse_args, save_json
-
-shows = load_shows(read_only=True)
 
 api_client = ApiV2Client('nadinasiddiquiwaz', 'ZEDKTMYBNB29LBOS', 'EISRLGJH035SO60Q')
 api_client.login()
 
-for show in shows.values():
+import sys
+
+sys.stdout = open('data/names.txt', 'w')
+
+
+def check_title(show: Series):
+    episodes = []
     if not show.tvdb_id:
-        continue
-    api = api_client.get_series(show.tvdb_id)
-    api_name = api['data']['seriesName']
-    if not show.series_name == api_name:
-        print(show.series_name, api_name, show.tvdb_id)
+        return
+    for i in range(1, 100):
+        eps = api_client.get_series_episodes(show.tvdb_id, episode_number=None, page=i)
+        if 'code' in eps:
+            break
+        episodes.extend(eps['data'])
+
+    if not episodes:
+        print(show.series_name)
+        return []
+    episodes = sorted(episodes, key=itemgetter('airedSeason', 'airedEpisodeNumber'))
+    for e in episodes:
+        ep = show.get_episode_by_sxe(e['airedSeason'], e['airedEpisodeNumber'])
+        if not ep:
+            continue
+        if not ep.get_title() == e['episodeName']:
+            print(ep)
+            print(ep.get_title())
+            print(e['episodeName'])
 
 
+SHOWS = load_shows(read_only=True)
 
+for s in sorted(list(SHOWS.values()), key=lambda k: k.series_name.lower()):
+    check_title(s)
