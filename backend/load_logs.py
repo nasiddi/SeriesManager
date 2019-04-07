f3cdd7331f2223b1331b69c2d64ba7ab24ae58f6
Nadina Siddiqui <siddinad@students.zhaw.ch>
2019-04-07 16:54:39 +0200

    added download logs

--- backend/load_logs.py
+++ backend/load_logs.py
@@ -8,7 +8,7 @@ def main(args, out_file='data/load_logs.json'):
     if args:
         parse_args(args)
         out_file = environ[OUT_FILE]
-    logs = [{'file:': f[:-5], 'data': load_json(path.join(LOG_DIR, f)), 'opened': True}
+    logs = [{'file': f[:-5], 'data': load_json(path.join(LOG_DIR, f)), 'opened': True}
             for f in listdir(LOG_DIR) if f.endswith('json')]
 
     print(logs)
